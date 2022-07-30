ddfbf6fa51b8a44cb4b7ad351281063f99ec17ff
AaryakPrasad <aaryakprasad@gmail.com>
2022-07-30 21:40:42 +0530

    Feat: Optimization

--- week2/2dDynamicArr.c
+++ week2/2dDynamicArr.c
@@ -4,15 +4,9 @@ int nonzero(int **a, int n)
 {
     int count = 0;
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
             if (a[i][j]!= 0)
-            {
                 count++;
-            }
-        }
-    }
     return count;
 }
 
@@ -20,15 +14,9 @@ int maindiagonaladd(int **a, int n)
 {
     int sum = 0;
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
             if (j>i)
-            {
                 sum+=a[i][j];
-            }
-        }
-    }
     return sum;
 }
 
@@ -37,28 +25,21 @@ int minordiagonaladd(int **a, int n)
     int sum = 0;
     int k = n-2;
     for (int i = 1; i <n;i++)
-    {
         for (int j = n-1; j > k;j--)
-        {
             sum = sum + a[i][j];
-        }
         k--;
-    }
     return sum;
 }
+
 int product(int **a, int n)
 {
     int product = 1;
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
             if (i==j)
                 product  *= a[i][j];
             else if ((i+j)==(n-1))
                 product  *= a[i][j];
-        }
-    }
     return product;
 }
 int main()
@@ -68,33 +49,18 @@ int main()
     scanf("%d", &n);
     int *arr[n];
     for (int i = 0; i < n; i++)
-    {
         arr[i] = (int *)malloc(n * sizeof(int));
-    }
+    
     printf("Enter the array: ");
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
-        {
             scanf("%d",&arr[i][j]);
-        }
-    }
-    printf("\nThe array is: \n");
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            printf("%d ",arr[i][j]);
-        }
-        printf("\n");
-    }
+    
     printf("\nThe number of nonzero elements is: %d",nonzero(arr,n));
     printf("\nThe sum of the elements above the main diagonal is: %d",maindiagonaladd(arr,n));
     printf("\nThe sum of the elements below the minor diagonal is: %d",minordiagonaladd(arr,n));
     printf("\nThe product of the elements in both the diagonals is: %d",product(arr,n));
     for (int i = 0; i < n; i++)
-    {
         free(arr[i]);
-    }
     return 0;
 }
